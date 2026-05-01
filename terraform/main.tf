e93f1d52531bacae36c1f3159b092e2f8c5f8824
udara88 <udarawickramasinghe88@gmail.com>
2026-05-02 05:08:35 +0530

    amended main file

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,7 +11,7 @@ terraform {
   # Remote state configuration
   backend "s3" {
     bucket = "sample-game-app-terraform-state-544719091850"
-    key    = "smile-game-frontend/terraform.tfstate"
+    key    = "sample-game-frontend/terraform.tfstate"
     region = "us-east-1"
   }
 }
